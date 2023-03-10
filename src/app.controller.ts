5b137fc4da5c66b5f1af04b8b750616fc5a9a690
kimhyunrip <steve@blockodyssey.io>
2023-03-10 17:18:38 +0900

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @EventPattern('lock')
+  public async lock(
+    @Body() req,
+  ){
+    console.log(req);
+  }
+}
